11eaa9ee012fa1ae1c3d5fb7a0f151c8adf66b02
lintao <lintao.zhang@thoughtworks.com>
2020-10-21 17:48:01 +0800

    [Lintao] fix: render page even thought there's no user data

--- src/components/ResumeHead/ResumeHead.js
+++ src/components/ResumeHead/ResumeHead.js
@@ -1,6 +1,8 @@
+import defaultImage from "../../assets/images/garfield.jpg";
+
 const ResumeHead = (user) => {
   return `<section class="resume-head">
-            <img src=${user.avatar} alt="" class="avatar">
+            <img src=${user ? user.avatar : defaultImage} alt="" class="avatar">
             <p class="say-hello">HELLO,</p>
             <p class="self-intro">
                 MY NAME IS <span class="user-name">${
