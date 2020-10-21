f34d60a135fce15b43f270298c23bb2b679c7903
lintao <lintao.zhang@thoughtworks.com>
2020-10-21 16:34:14 +0800

    [Lintao] fix: change user avatar to it's url

--- src/components/ResumeHead/ResumeHead.js
+++ src/components/ResumeHead/ResumeHead.js
@@ -1,8 +1,6 @@
-import avatar from "../../assets/images/garfield.jpg";
-
 const ResumeHead = (user) => {
   return `<section class="resume-head">
-            <img src=${avatar} alt="" class="avatar">
+            <img src=${user.avatar} alt="" class="avatar">
             <h1 class="say-hello">HELLO,</h1>
             <p>
                 MY NAME IS <span class="user-name">${
