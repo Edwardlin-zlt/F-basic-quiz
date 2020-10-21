89cd840b61a6c2173a75342b51f5aa8a5ea3d2b7
lintao <lintao.zhang@thoughtworks.com>
2020-10-21 14:18:43 +0800

    [Lintao] feat: add education component

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import ResumeHead from "./component/ResumeHead/ResumeHead";
 import AboutMe from "./component/AboutMe/AboutMe";
+import Educations from "./component/Education/Education";
 
 const renderMain = async () => {
   const main = document.querySelector(".main");
   main.insertAdjacentHTML("beforeend", await ResumeHead());
   main.insertAdjacentHTML("beforeend", await AboutMe());
+  main.insertAdjacentHTML("beforeend", await Educations());
 };
 
 renderMain();
---
+++ src/component/Education/Education.js
@@ -0,0 +1,12 @@
+const Educations = async () => {
+  return `
+      <section class="education">
+        <h1 class="education-title">EDUCATION</h1>
+        <ul class="education-container">
+            Education
+        </ul>
+      </section>
+    `;
+};
+
+export default Educations;
