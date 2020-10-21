794540deda48fc662346039421cccadd9986fbcd
lintao <lintao.zhang@thoughtworks.com>
2020-10-21 12:23:02 +0800

    [Lintao] feat: Backend and Frontend connected

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
-import "./style/index.scss";
+import UserApi from "./api/user/user";
+
+const renderMain = async () => {
+  const main = document.querySelector(".main");
+  const users = await UserApi.getUsers();
+  console.log(users);
+  main.innerHTML = JSON.stringify(users[0]);
+};
+
+renderMain();
