13ad13878c9ab2d03a9df142626aabcdd3c184fd
lintao <lintao.zhang@thoughtworks.com>
2020-10-21 18:15:59 +0800

    [Lintao] feat: add ResumeHead test

--- src/components/ResumeHead/ResumeHead.js
+++ src/components/ResumeHead/ResumeHead.js
@@ -1,8 +1,6 @@
-import defaultImage from "../../assets/images/garfield.jpg";
-
 const ResumeHead = (user) => {
   return `<section class="resume-head">
-            <img src=${user ? user.avatar : defaultImage} alt="" class="avatar">
+            <img src=${user ? user.avatar : ""} alt="" class="avatar">
             <p class="say-hello">HELLO,</p>
             <p class="self-intro">
                 MY NAME IS <span class="user-name">${
