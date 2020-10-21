a3bed963a8c29f9ad41de954bf96c658995e3394
lintao <lintao.zhang@thoughtworks.com>
2020-10-21 15:25:47 +0800

    [Lintao] feat: render educations in resume page

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -2,24 +2,35 @@ import UserApi from "../api/user/user";
 import ResumeHead from "../components/ResumeHead/ResumeHead";
 import AboutMe from "../components/AboutMe/AboutMe";
 import Educations from "../components/Education/Education";
+import EducationApi from "../api/education/education";
 
 class Resume {
   getUser = async () => {
-    const curUrl = window.location.href;
-    const userId = curUrl.split("/").pop();
+    const userId = this.getUserId();
     return UserApi.getUserById(userId);
+  };
+
+  getEducations = async () => {
+    const userId = this.getUserId();
+    return EducationApi.getEducationsByUserId(userId);
+  };
+
+  getUserId = () => {
+    const curUrl = window.location.href;
+    return curUrl.split("/").pop();
   };
 
   pageDidMount = async () => {
     this.user = await this.getUser();
+    this.educations = await this.getEducations();
   };
 
   render = async () => {
     await this.pageDidMount();
-    const { user } = this;
+    const { user, educations } = this;
     return `${ResumeHead(user)}
                 ${AboutMe(user)}
-                ${Educations()}`;
+                ${Educations(educations)}`;
   };
 }
 
